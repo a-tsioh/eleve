0b69ee328320c934e6caebcf76eb5efd679265d0
Pierre M <pierre.magistry@univ-amu.fr>
2020-11-03 01:16:54 +0100

    ...

--- eleve/__init__.py
+++ eleve/__init__.py
@@ -6,13 +6,15 @@ import warnings
 
 __version__ = "20.10"
 
-__all__ = [
-    "MemoryStorage",
-    "Segmenter",
-    "CSVStorage",
-    "preprocessing",
-    "utils"
-]
+# __all__ = [
+#     "MemoryStorage",
+#     "Segmenter",
+#     "CSVStorage",
+#     "preprocessing",
+#     "utils"
+# ]
 
 from eleve.segment import Segmenter
 from eleve.memory import MemoryStorage, CSVStorage
+# import preprocessing
+# import utils
