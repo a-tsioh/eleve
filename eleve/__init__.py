59bc6a71a78e8faaf71a085269872985241ca2d1
Pierre M <pierre.magistry@univ-amu.fr>
2020-11-03 01:04:22 +0100

    ...

--- eleve/__init__.py
+++ eleve/__init__.py
@@ -16,5 +16,5 @@ __all__ = [
 
 from eleve.segment import Segmenter
 from eleve.memory import MemoryStorage, CSVStorage
-#from . import preprocessing
-#from . import utils
+from . import preprocessing
+from . import utils
